b46587309ae73ddcdec913d3cbc743bb186d1813
Thomas <thomas02.debray@gmail.com>
2024-03-11 15:19:33 +0100

    netflix5000Thomas

---
+++ netflix5000/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SignUpView from './views/SignUpView.vue';
+</script>
+
+<template>
+<SignUpView></SignUpView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ netflix5000/src/views/SignUpView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import baseBackground from '@/components/baseBackground.vue';
+</script>
+
+<template>
+<baseBackground>
+<div class="modal"> 
+  <p>Sign Up</p>
+  <input type="text">
+  <input type="text">
+  <input type="text">
+  <button></button>
+</div>
+</baseBackground>
+</template>
+
+<style scoped>
+baseBackgroud {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal {
+  height: 500px;
+  width: 300px;
+  z-index: 1;
+  background-color: hsla(0, 0%, 11%, 0.767);
+}
+</style>
