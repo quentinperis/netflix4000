a052335876c8733ba38dcc112624075b09cfb225
Thomas <thomas02.debray@gmail.com>
2024-03-12 11:19:59 +0100

    first Netflix8k

---
+++ Netflix8k/netflix2000/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SignInView from './views/SignInView.vue';
+</script>
+
+<template>
+<SignInView></SignInView>
+</template>
+
+<style scoped>
+
+</style>
