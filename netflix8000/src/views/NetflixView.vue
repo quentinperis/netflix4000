057115c9dffbce12713fb2be907528343baefb0c
elias <eliaslallouet59@gmail.com>
2024-05-02 09:43:37 +0200

    add netflix elias

--- netflix8000/src/views/NetflixView.vue
+++ netflix8000/src/views/NetflixView.vue
@@ -26,7 +26,7 @@ const closeVideo = () => {
       <div>
         <h1 class="jokertitle">Watch Joker Now</h1>
         <p class="joker-synopsis">
-          Forever alone in a crowd, failed comedian Arthur Fleck seeks
+          forever alone in a crowd, failed comedian Arthur Fleck seeks
           connection as he walks the streets of Gotham City. Arthur wears two
           masks -- the one he paints for his day job as a clown, and the guise
           he projects in a futile attempt to feel like he's part of the world
